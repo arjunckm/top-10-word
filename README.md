3d81bf03292675babd62fbb9b8fbdcb39800ef64
Arjun JS <arjunckm@yahoo.com>
2021-02-20 12:32:25 +0530

    final push

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-##Top 10 word count
+## Top 10 word count
+### Description
+1. Fetch document from given url http://norvig.com/big.txt
+2. Analyse the document using asynchronous mechanism, fetched in step 1
+  * a. Find occurrences count of word in document
+  * b. Collect details for top 10 words(order by word Occurrences) from
+  https://dictionary.yandex.net/api/v1/dicservice.json/lookup, check details of
+  API given below
+    * i. synonyms/means
+    * ii. part Of Speech/pos
+
+3. Show words list in JSON format for top 10 words.
+  * a. Word: text
+  * b. Output
+    * i. Count of Occurrence in that Particular Document
+    * ii. Synonyms
+    * iii. Pos
+## How to run?
+1. Git clone the repo. " git clone https://github.com/arjunckm/top-10-word.git"
+2. Enter inside repo and run  "npm install"
+3. now just run "node index.js"
+4. check output in terminal or check generated output.json file.
--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 const got = require('got');
-const APIkey="dict.1.1.20210216T114936Z.e4989dccd61b9626.373cddfbfb8a3b2ff30a03392b4e0b076f14cff9"
+const fs = require('fs');
+const APIkey="dict.1.1.20210216T114936Z.e4989dccd61b9626.373cddfbfb8a3b2ff30a03392b4e0b076f14cff9";
 
 
-
-const getInitialFile = (async () => {
+(async () => {
     const fileUrl="http://norvig.com/big.txt";
 	try {
 		const response = await got(fileUrl);
-		let fileContent = response.body;        
-        getWordFrequency(fileContent,10);
+		let fileContent = response.body;  
+        // console.log(fileContent);      
+        let finalResult = await getWordFrequency(fileContent,10);
+        //console.log(finalResult);
 	} catch (err) {
 		console.log(err.response.body);
 	}
-});
+})();
 
 const getDetailsOfWord = ( async (wordSent)=>{
     try{
         const wordDirUrl=`https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=${APIkey}&lang=en-en&text=${wordSent}`;
+        //console.log(wordDirUrl);
         
         const wordData = await got(wordDirUrl);
-        console.log(wordData.body);
-
+        //console.log(wordData.body);
+        return wordData.body;
     } catch (err){
         console.log(err.response.body);
     }
@@ -28,23 +31,72 @@ const getDetailsOfWord = ( async (wordSent)=>{
 })
 
 const getWordFrequency= (async(fileContent, noOfWords)=>{
+    var promise = new Promise((resolve, reject) => {
     //remove symbols and clean file content;    
-    let updatedContent = fileContent.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, '');
+    let updatedContent = fileContent.replace(/[.,-/#!$%^&*;:{}=\-_`~()?<>'"]/g, ''); // /[&\/\\#,-_+()!$~%^.'";:*?=<>{}]/g
     updatedContent = (updatedContent.replace(/\n/g, ' ')).toLowerCase();
 
     //split updatedContent and creat array of words
-    let words = updatedContent.split(' ') .filter(val => /\S/.test(val));
+    let wordList = updatedContent.split(' ') .filter(val => /\S/.test(val));
 
     let wordCount={};
-    words.forEach((word)=>{
+    wordList.forEach((word)=>{
         wordCount[word] = wordCount[word] || 0;
         wordCount[word]++;
     })
 
+    let topWords = Object.keys(wordCount).sort(function(a, b){ return wordCount[b]- wordCount[a]});
+    topWords = topWords.slice(0, noOfWords);
 
-    console.log(wordCount);
+    let finalOutput=[];
+        
+        topWords.forEach(async(word,i, array) =>{
+            let detailsOfWord = await getDetailsOfWord(word);
+            //console.log(`word is ${word} :`, detailsOfWord);
+            let wordDetails = JSON.parse(detailsOfWord);
+            let singleResult = {
+                "wordCount": wordCount[word]
+            }
 
+            if (wordDetails.def[0]) {
+                if ("syn" in wordDetails.def[0]) {
+                    singleResult.synonyms = wordDetails.def[0].syn;
+                } else {
+                    if ("mean" in wordDetails.def[0]) {
+                        singleResult.synonyms = wordDetails.def[0].mean;
+                    } else {
+                        singleResult.synonyms = "Synonyms not found";
+                    }
+                }
+                if ("pos" in wordDetails.def[0]) {
+                    singleResult.pos = wordDetails.def[0].pos;
+                } else {
+                    singleResult.pos = "POS not found";
+                }
+            } else {
+                singleResult.synonyms = "Synonyms not found";
+                singleResult.pos = "POS not found";
+            }
+            // console.log(singleResult);
+            finalOutput.push({
+                word,
+                "output":singleResult
+            });
+            if (i === array.length -1) {
+                setTimeout(()=>{
+                    resolve(finalOutput);
+                },1000)
+            }      
+        })       
+    });
+
+    promise.then((outputs) => {
+        outputs = outputs.sort(function (a, b) {
+            return b.output.wordCount - a.output.wordCount
+        })
+        fs.writeFileSync('output.json', JSON.stringify(outputs));
+        console.log(outputs);
+    });
 })
 
-//getDetailsOfWord('the');
-getInitialFile();
+// getDetailsOfWord('was');
